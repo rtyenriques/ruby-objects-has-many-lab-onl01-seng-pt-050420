7c9063172daa638c28f5187ff117d82263b7a602
Ross Enriques <rtyenriques@gmail.com>
2020-06-03 00:22:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,6 +3,7 @@ class Author
 
   attr_accessor :name, :posts
 
+ @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -11,6 +12,8 @@ class Author
 
   def add_post(post)
     @posts << post
+    @@post_count += 1
+
   end
 
   def posts
@@ -22,6 +25,8 @@ class Author
      post = Post.new(title)
      @posts << post
      post.author = self
+     @@post_count += 1
+
     
    end
 
